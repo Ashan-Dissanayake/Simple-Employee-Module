23c0fac58ba2da7b727c816cdd97f506eb45e220
Ashan Dissanayake <ashanpathum899@gmail.com>
2024-03-06 21:24:27 +0530

    Update EmployeeUi.java
    Remove SOUT from UI Class

--- src/main/java/lk/ashan/view/EmployeeUi.java
+++ src/main/java/lk/ashan/view/EmployeeUi.java
@@ -478,8 +478,6 @@ public class EmployeeUi extends JFrame {
 
     public void btnUpdateAp(ActionEvent e) {
 
-        // System.out.println("Test-OK");
-
         String errors = getErrors();
         employee.setId(oldEmployee.getId());
 
